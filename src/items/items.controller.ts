5cbe7c6eb14cdd2a22e915cdde4d65d7e2363852
Gabriel de Avila Garrido <gabrielgarridoag@gmail.com>
2024-08-26 19:42:13 -0300

    Adicionando operações faltantes do CRUD

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item-dto';
 
 @Controller('items')
@@ -8,8 +16,26 @@ export class ItemsController {
     return 'Get all items';
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `Get item ${id}`;
+  }
+
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc: ${createItemDto.description} Qtd: ${createItemDto.quantity}`;
+  }
+
+  @Put(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateItemDto: CreateItemDto,
+  ): string {
+    return `Update item ${id} Name: ${updateItemDto.name} Desc: ${updateItemDto.description} Qtd: ${updateItemDto.quantity}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): string {
+    return `Delete item ${id}`;
   }
 }
